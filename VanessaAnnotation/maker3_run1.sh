0dd715869c234d63edf529e2269eae979117cef3
DaSh-bash <70020998+DaSh-bash@users.noreply.github.com>
2021-04-17 20:34:12 +0200

    Update maker3_run1.sh

--- VanessaAnnotation/maker3_run1.sh
+++ VanessaAnnotation/maker3_run1.sh
@@ -4,15 +4,15 @@
 #SBATCH -p core
 #SBATCH -n 8
 #SBATCH -t 50:00:00
-#SBATCH -J Maker1
-#SBATCH -o Maker1.out
-#SBATCH -e Maker1.error
+#SBATCH -J maker3_run1
+#SBATCH -o maker3_run1.out
+#SBATCH -e maker3_run1.error
 #SBATCH --mail-user daria.shipilina@gmail.com
 #SBATCH --mail-type=ALL
 
 module load bioinfo-tools maker
-MAKERDIR="/proj/uppstore2017185/b2014034_nobackup/Dasha/Vanessa_MAKER/maker3_run"
+MAKERDIR="maker3_run1"
 
-maker -base ${MAKERDIR} -fix_nucleotides -dsindex
+maker -base ${MAKERDIR} -fix_nucleotides
 gff3_merge  -d ${MAKERDIR}.maker.output/${MAKERDIR}_master_datastore_index.log
 fasta_merge -d ${MAKERDIR}.maker.output/${MAKERDIR}_master_datastore_index.log
