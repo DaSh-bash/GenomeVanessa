87f26094f127e183d101da90c30c27ae35bf774b
Daria Shipilina <daria@rackham2.uppmax.uu.se>
2021-07-09 12:06:36 +0200

    gene_selection

---
+++ VanessaAnnotation/gene_selector.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+awk '{print $2}' $1 | tr -d \" | tail -n +2 > $1.tmp
+
+grep -Fwf $1.tmp Vcardui_Orthogroups.tsv | fmt -1 > $1_Genes_OGs.tmp
+sed '/OG/d' $1_Genes_OGs.tmp  |  tr -d , 
+#rm *tmp
